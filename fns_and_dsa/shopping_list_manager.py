387884664da4fb318bdf5cfefb6615de36620f48
Clintina <clintinaroberts22@gmail.com>
2025-06-09 10:17:58 +0200

    Debugged again

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,10 +1,10 @@
 def display_menu():
     """Displays the shopping list menu."""
-    print("\Shopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
+    print(f"Shopping List Manager")
+    print(f"1. Add Item")
+    print(f"2. Remove Item")
+    print(f"3. View List")
+    print(f"4. Exit")
 
 def main():
     shopping_list = []  # Initialize an empty list
