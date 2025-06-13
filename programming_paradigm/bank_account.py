0b31c464fb97a1683339d16596247803f20f1cac
Clintina <clintinaroberts22@gmail.com>
2025-06-13 04:40:48 +0200

    Really Debugged Double insufficient funds error

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,8 +15,7 @@ class BankAccount:
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
             return True
-        else:
-            print("Insufficient funds.")
+        
             return False
 
     def display_balance(self):
