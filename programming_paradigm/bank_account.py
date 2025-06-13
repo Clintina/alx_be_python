f98f89648ac9e8ae3fc802f2a47affe36d6a19fc
Clintina <clintinaroberts22@gmail.com>
2025-06-13 04:22:51 +0200

    Finally fixed double insufficient funds error

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -21,10 +21,3 @@ class BankAccount:
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
-# ...existing code...
-
-if __name__ == "__main__":
-    account = BankAccount(100)
-    account.deposit(50)
-    account.withdraw(30)
-    account.display_balance()
